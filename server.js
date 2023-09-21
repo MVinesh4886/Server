7bbb44b38a5f569e7c5a8ca325909962fd48f3cc
MVinesh4886 <vineshkrishna26@gmail.com>
2023-09-21 22:59:02 +0530

    create login html File

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ console.log(http);*/
 // Now you can see bunch of methods in the command prompt
 
 const http = require("http");
+const fs = require("fs");
 
 //! create server
 const server = http.createServer(function (req, res) {
@@ -20,5 +21,20 @@ const server = http.createServer(function (req, res) {
 server.listen(7000, () => {
   console.log("server is up and running");
 });
+
+//create login html file
+fs.writeFile(
+  "login.html",
+  `<div><h1>This is login Page</h1>
+<p> Please provide your valid login credentials</P></div>
+`,
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("File has been created");
+    }
+  }
+);
 
 //*local host: In computer networking localhost is a hostname that refers to the current device used to access it.Here, "7000 " is the port number. And we can access with the url: "http://localhost:7000"
